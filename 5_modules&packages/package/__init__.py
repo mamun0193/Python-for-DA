e7834995a37616b7e505b1061ae037a6f1e18d79
mamun0193 <mamunrahaman019@gmail.com>
2026-07-10 23:28:00 +0530

    Add modules,packages and standard library notebook

---
+++ 5_modules&packages/package/__init__.py
@@ -0,0 +1,5 @@
+''' __init__.py is a special Python file that is used to indicate that the directory it is present in is a Python package. 
+It can be an empty file or can contain initialization code for the package. When a package is imported, the __init__.py file is executed,
+allowing you to define package-level variables, functions, or classes.
+In Python 3.3 and later, the __init__.py file is not strictly required for a directory to be recognized as a package, 
+but it is still commonly used for backward compatibility and to provide package-level functionality.'''
